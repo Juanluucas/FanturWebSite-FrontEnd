8bc7863e02103df672b7c0048e1e0f326c40c4d3
Juanluucas <juanlucasbiain@gmail.com>
2022-06-06 09:40:24 -0300

    comentarios mas acordes al proyecto, fases del mundial arreglado

--- src/views/Home.js
+++ src/views/Home.js
@@ -46,9 +46,9 @@ function Home() {
                               {/* <!-- ***** Menu Start ***** --> */}
                               <ul class="nav">
                                   <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
-                                  <li ><a href="/CatalogoPaquetes">Catalogo Paquetes</a></li>
                                   <li class="scroll-to-section"><a href="#features">Nuestros Servicios</a></li>
-                                  <li class="scroll-to-section"><a href="#our-classes">Classes</a></li>
+                                  <li class="scroll-to-section"><a href="#our-classes">Fases</a></li>
+                                  <li ><a href="/CatalogoPaquetes">Catalogo Paquetes</a></li>
                                   <li ><a href="/Conocenos">Conocenos</a></li> 
                                   <li class="main-button"><a href="/#">Sign Up</a></li>
                               </ul>        
@@ -84,14 +84,14 @@ function Home() {
                       <h6>Copa Mundial de la FIFA Catar 2022</h6>
                       <h2>Donde vivirás el <em>Fútbol</em></h2>
                       <div class="main-button scroll-to-section">
-                          <a href="#features">Become a member</a>
+                          <a href="#features">Nuestros Servicios</a>
                       </div>
                   </div>
               </div>
           </div>
           {/* <!-- ***** Main Banner Area End ***** --> */}
 
-          {/* <!-- ***** Features Item Start ***** --> */}
+          {/* <!-- ***** SERVICIOS ESTRELLAS ***** --> */}
           <section class="section" id="features">
               <div class="container">
                   <div class="row">
@@ -181,10 +181,10 @@ function Home() {
                   <div class="row">
                       <div class="col-lg-10 offset-lg-1">
                           <div class="cta-content">
-                              <h2>NO LO <em>PIENSES</em>, COMPRA <em>YA</em>!</h2>
-                              <p>Es el ultimo mundial de Messi, te lo vas a perder??? .</p>
+                              <h2>NO LO <em>PIENSES</em>, VIVE LA <em>EXPERIENCIA</em>!</h2>
+                              <p>El torneo mas importante de deportes a nivel Mundial, solo ocurre cada 4 años, vas a salteartelo?  .</p>
                               <div class="main-button scroll-to-section">
-                                  <a href="#our-classes">Become a member</a>
+                                  <a href="/CatalogoPaquetes">Ver Paquetes</a>
                               </div>
                           </div>
                       </div>
@@ -193,7 +193,7 @@ function Home() {
           </section>
           {/* <!-- ***** Call to Action End ***** --> */}
 
-          {/* <!-- ***** Our Classes Start ***** --> */}
+          {/* <!-- ***** FASES DEL MUNDIAL ***** --> */}
           <section class="section" id="our-classes">
               <div class="container">
                   <div class="row">
@@ -212,124 +212,71 @@ function Home() {
                         <li><a href='#tabs-2'><img src={require("../assets/images/tabs-first-icon.png")} alt=""/>Octavos de final</a></li>
                         <li><a href='#tabs-3'><img src={require("../assets/images/tabs-first-icon.png")} alt=""/>Cuartos de final</a></li>
                         <li><a href='#tabs-4'><img src={require("../assets/images/tabs-first-icon.png")} alt=""/>Semifinal</a></li>
-                        <li><a href='#tabs-4'><img src={require("../assets/images/tabs-first-icon.png")} alt=""/>final</a></li>
-                        <div class="main-rounded-button"><a href="/#">View All Schedules</a></div>
+                        <li><a href='#tabs-5'><img src={require("../assets/images/tabs-first-icon.png")} alt=""/>final</a></li>
+                        <div class="main-rounded-button"><a href="/CatalogoPaquetes">Vea nuestros Paquetes</a></div>
                       </ul>
                     </div>
                     <div class="col-lg-8">
                       <section class='tabs-content'>
                         <article id='tabs-1'>
-                          <img src={require("../assets/images/training-image-01.jpg")} alt="First Class"/>
-                          <h4>First Training Class</h4>
+                          <h4>Fases de grupos</h4>
                           <p>Phasellus convallis mauris sed elementum vulputate. Donec posuere leo sed dui eleifend hendrerit. Sed suscipit suscipit erat, sed vehicula ligula. Aliquam ut sem fermentum sem tincidunt lacinia gravida aliquam nunc. Morbi quis erat imperdiet, molestie nunc ut, accumsan diam.</p>
-                          <div class="main-button">
-                              <a href="/#">View Schedule</a>
+                            <div class="main-button">
+                              <a href="/#">paquetes filtrados?</a>
                           </div>
+                          <img src={require("../assets/images/training-image-01.jpg")} alt="First Class"/>
+                          
                         </article>
                         <article id='tabs-2'>
-                          <img src={require("../assets/images/training-image-02.jpg")} alt="Second Training"/>
-                          <h4>Second Training Class</h4>
-                          <p>Integer dapibus, est vel dapibus mattis, sem mauris luctus leo, ac pulvinar quam tortor a velit. Praesent ultrices erat ante, in ultricies augue ultricies faucibus. Nam tellus nibh, ullamcorper at mattis non, rhoncus sed massa. Cras quis pulvinar eros. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                          <div class="main-button">
-                              <a href="/#">View Schedule</a>
+                          <h4>Octavos de final</h4>
+                          <p>Phasellus convallis mauris sed elementum vulputate. Donec posuere leo sed dui eleifend hendrerit. Sed suscipit suscipit erat, sed vehicula ligula. Aliquam ut sem fermentum sem tincidunt lacinia gravida aliquam nunc. Morbi quis erat imperdiet, molestie nunc ut, accumsan diam.</p>
+                            <div class="main-button">
+                              <a href="/#">paquetes filtrados?</a>
                           </div>
+                          <img src={require("../assets/images/training-image-01.jpg")} alt="First Class"/>
+                          
                         </article>
                         <article id='tabs-3'>
-                          <img src={require("../assets/images/training-image-03.jpg")} alt="Third Class"/>
-                          <h4>Third Training Class</h4>
-                          <p>Fusce laoreet malesuada rhoncus. Donec ultricies diam tortor, id auctor neque posuere sit amet. Aliquam pharetra, augue vel cursus porta, nisi tortor vulputate sapien, id scelerisque felis magna id felis. Proin neque metus, pellentesque pharetra semper vel, accumsan a neque.</p>
-                          <div class="main-button">
-                              <a href="/#">View Schedule</a>
+                          <h4>Cuartos de final</h4>
+                          <p>Phasellus convallis mauris sed elementum vulputate. Donec posuere leo sed dui eleifend hendrerit. Sed suscipit suscipit erat, sed vehicula ligula. Aliquam ut sem fermentum sem tincidunt lacinia gravida aliquam nunc. Morbi quis erat imperdiet, molestie nunc ut, accumsan diam.</p>
+                            <div class="main-button">
+                              <a href="/#">paquetes filtrados?</a>
                           </div>
+                          <img src={require("../assets/images/training-image-01.jpg")} alt="First Class"/>
+                          
                         </article>
                         <article id='tabs-4'>
-                          <img src={require("../assets/images/training-image-04.jpg")} alt="Fourth Training"/>
-                          <h4>Fourth Training Class</h4>
-                          <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultrices elementum odio ac tempus. Etiam eleifend orci lectus, eget venenatis ipsum commodo et.</p>
-                          <div class="main-button">
-                              <a href="/#">View Schedule</a>
+                          <h4>Semifinal</h4>
+                          <p>Phasellus convallis mauris sed elementum vulputate. Donec posuere leo sed dui eleifend hendrerit. Sed suscipit suscipit erat, sed vehicula ligula. Aliquam ut sem fermentum sem tincidunt lacinia gravida aliquam nunc. Morbi quis erat imperdiet, molestie nunc ut, accumsan diam.</p>
+                            <div class="main-button">
+                              <a href="/#">paquetes filtrados?</a>
                           </div>
+                          <img src={require("../assets/images/training-image-01.jpg")} alt="First Class"/>
+                          
+                        </article>
+                        <article id='tabs-5'>
+                          <h4>Final</h4>
+                          <p>Phasellus convallis mauris sed elementum vulputate. Donec posuere leo sed dui eleifend hendrerit. Sed suscipit suscipit erat, sed vehicula ligula. Aliquam ut sem fermentum sem tincidunt lacinia gravida aliquam nunc. Morbi quis erat imperdiet, molestie nunc ut, accumsan diam.</p>
+                            <div class="main-button">
+                              <a href="/#">paquetes filtrados?</a>
+                          </div>
+                          <img src={require("../assets/images/training-image-01.jpg")} alt="First Class"/>
+                          
                         </article>
                       </section>
                     </div>
                   </div>
               </div>
           </section>
-          {/* // <!-- ***** Our Classes End ***** --> */}
           
-          <section class="section" id="schedule">
+          
+          <section class="section" id="schedule">        
               <div class="container">
                   <div class="row">
-                      <div class="col-lg-6 offset-lg-3">
-                          <div class="section-heading dark-bg">
-                              <h2>Classes <em>Schedule</em></h2>
+                      <div class="col-lg-6 offset-lg-3 section-heading">
+                              <h2>aca va  <em>conocenos /hacer css lindo aca</em></h2>
                               <img src={require("../assets/images/line-dec.png")} alt=""/>
-                              <p>Nunc urna sem, laoreet ut metus id, aliquet consequat magna. Sed viverra ipsum dolor, ultricies fermentum massa consequat eu.</p>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="row">
-                      <div class="col-lg-12">
-                          <div class="filters">
-                              <ul class="schedule-filter">
-                                  <li class="active" data-tsfilter="monday">Monday</li>
-                                  <li data-tsfilter="tuesday">Tuesday</li>
-                                  <li data-tsfilter="wednesday">Wednesday</li>
-                                  <li data-tsfilter="thursday">Thursday</li>
-                                  <li data-tsfilter="friday">Friday</li>
-                              </ul>
-                          </div>
-                      </div>
-                      <div class="col-lg-10 offset-lg-1">
-                          <div class="schedule-table filtering">
-                              <table>
-                                  <tbody>
-                                      <tr>
-                                          <td class="day-time">Fitness Class</td>
-                                          <td class="monday ts-item show" data-tsmeta="monday">10:00AM - 11:30AM</td>
-                                          <td class="tuesday ts-item" data-tsmeta="tuesday">2:00PM - 3:30PM</td>
-                                          <td>William G. Stewart</td>
-                                      </tr>
-                                      <tr>
-                                          <td class="day-time">Muscle Training</td>
-                                          <td class="friday ts-item" data-tsmeta="friday">10:00AM - 11:30AM</td>
-                                          <td>Paul D. Newman</td>
-                                      </tr>
-                                      <tr>
-                                          <td class="day-time">Body Building</td>
-                                          <td class="tuesday ts-item" data-tsmeta="tuesday">10:00AM - 11:30AM</td>
-                                          <td class="monday ts-item show" data-tsmeta="monday">2:00PM - 3:30PM</td>
-                                          <td>Boyd C. Harris</td>
-                                      </tr>
-                                      <tr>
-                                          <td class="day-time">Yoga Training Class</td>
-                                          <td class="wednesday ts-item" data-tsmeta="wednesday">10:00AM - 11:30AM</td>
-                                          <td class="friday ts-item" data-tsmeta="friday">2:00PM - 3:30PM</td>
-                                          <td>Hector T. Daigle</td>
-                                      </tr>
-                                      <tr>
-                                          <td class="day-time">Advanced Training</td>
-                                          <td class="thursday ts-item" data-tsmeta="thursday">10:00AM - 11:30AM</td>
-                                          <td class="wednesday ts-item" data-tsmeta="wednesday">2:00PM - 3:30PM</td>
-                                          <td>Bret D. Bowers</td>
-                                      </tr>
-                                  </tbody>
-                              </table>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </section>
-
-          <section class="section" id="trainers">
-              <div class="container">
-                  <div class="row">
-                      <div class="col-lg-6 offset-lg-3">
-                          <div class="section-heading">
-                              <h2>Expert <em>Trainers</em></h2>
-                              <img src={require("../assets/images/line-dec.png")} alt=""/>
-                              <p>Nunc urna sem, laoreet ut metus id, aliquet consequat magna. Sed viverra ipsum dolor, ultricies fermentum massa consequat eu.</p>
-                          </div>
+                              <p>Nunc urna sem, laoreet ut metus id, aliquet consequat magna. Sed viverra ipsum dolor, ultricies fermentum massa consequat eu.</p>                      
                       </div>
                   </div>
                   <div class="row">
@@ -389,7 +336,10 @@ function Home() {
                       </div>
                   </div>
               </div>
+              
           </section>
+
+         
           {/* <!-- ***** Testimonials Ends ***** --> */}
           
           {/* <!-- ***** Contact Us Area Starts ***** --> */}
