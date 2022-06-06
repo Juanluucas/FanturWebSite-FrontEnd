0ee6ad9f181404820a0ae763f4310006b8800ef8
Juanluucas <juanlucasbiain@gmail.com>
2022-06-06 00:52:52 -0300

    ajustando navbar y home

--- src/views/Home.js
+++ src/views/Home.js
@@ -41,15 +41,15 @@ function Home() {
                       <div class="col-12">
                           <nav class="main-nav">
                               {/* <!-- ***** Logo Start ***** --> */}
-                              <a href="index.html" class="logo">Training<em> Studio</em></a>
+                              <a href="/" class="logo">FAN<em> TUR</em></a>
                               {/* <!-- ***** Logo End ***** --> */}
                               {/* <!-- ***** Menu Start ***** --> */}
                               <ul class="nav">
                                   <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                                  <li ><a href="/CatalogoPaquetes">Catalogo Paquetes</a></li>
                                   <li class="scroll-to-section"><a href="#features">About</a></li>
                                   <li class="scroll-to-section"><a href="#our-classes">Classes</a></li>
-                                  <li class="scroll-to-section"><a href="#schedule">Schedules</a></li>
-                                  <li class="scroll-to-section"><a href="#contact-us">Contact</a></li> 
+                                  <li ><a href="/Conocenos">Conocenos</a></li> 
                                   <li class="main-button"><a href="/#">Sign Up</a></li>
                               </ul>        
                               {/* <a class='menu-trigger'>
@@ -396,42 +396,7 @@ function Home() {
               <div class="container-fluid">
                   <div class="row">
                       <div class="col-lg-6 col-md-6 col-xs-12">
-                          {/* <div id="map">
-                            <iframe src="https://maps.google.com/maps?q=Av.+L%C3%BAcio+Costa,+Rio+de+Janeiro+-+RJ,+Brazil&t=&z=13&ie=UTF8&iwloc=&output=embed" width="100%" height="600px" frameborder="0" style="border:0" allowfullscreen></iframe>
-                          </div> */}
-                      </div>
-                      <div class="col-lg-6 col-md-6 col-xs-12">
-                          <div class="contact-form">
-                              <form id="contact" action="" method="post">
-                                <div class="row">
-                                  <div class="col-md-6 col-sm-12">
-                                    <fieldset>
-                                      <input name="name" type="text" id="name" placeholder="Your Name*" required=""/>
-                                    </fieldset>
-                                  </div>
-                                  <div class="col-md-6 col-sm-12">
-                                    <fieldset>
-                                      <input name="email" type="text" id="email" pattern="[^ @]*@[^ @]*" placeholder="Your Email*" required=""/>
-                                    </fieldset>
-                                  </div>
-                                  <div class="col-md-12 col-sm-12">
-                                    <fieldset>
-                                      <input name="subject" type="text" id="subject" placeholder="Subject"/>
-                                    </fieldset>
-                                  </div>
-                                  <div class="col-lg-12">
-                                    <fieldset>
-                                      <textarea name="message" rows="6" id="message" placeholder="Message" required=""></textarea>
-                                    </fieldset>
-                                  </div>
-                                  <div class="col-lg-12">
-                                    <fieldset>
-                                      <button type="submit" id="form-submit" class="main-button">Send Message</button>
-                                    </fieldset>
-                                  </div>
-                                </div>
-                              </form>
-                          </div>
+                         
                       </div>
                   </div>
               </div>
